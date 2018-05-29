3b3e56498c4d16589167deb323950a1b4b06f011
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-05-29 11:45:04 -0300

    vehicle form

--- vega/ClientApp/app/app.shared.module.ts
+++ vega/ClientApp/app/app.shared.module.ts
@@ -9,10 +9,12 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        VehicleFormComponent,
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
@@ -24,6 +26,7 @@ import { CounterComponent } from './components/counter/counter.component';
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'vehicles/new', component: VehicleFormComponent},
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
