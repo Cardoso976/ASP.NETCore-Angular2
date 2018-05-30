8e571d2d1ba2436a1fec84d588a91f905bef07d9
Rodrigo <rodrigo_cardoso97@hotmail.com>
2018-05-29 23:11:04 -0300

    server-side validation

--- vega/Controllers/VehiclesController.cs
+++ vega/Controllers/VehiclesController.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using vega.Controllers.Resources;
 using vega.Models;
+using vega.Persistence;
 
 namespace vega.Controllers
 {
@@ -9,17 +12,37 @@ namespace vega.Controllers
     public class VehiclesController : Controller
     {
         private readonly IMapper mapper;
+        private readonly VegaDbContext context;
 
-        public VehiclesController(IMapper mapper)
+        public VehiclesController(IMapper mapper, VegaDbContext context)
         {
             this.mapper = mapper;
+            this.context = context;
         }
 
         [HttpPost]
-        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        public async Task<IActionResult> CreateVehicleAsync([FromBody] VehicleResource vehicleResource)
         {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState); 
+
+            /*Validation */
+            // var model = await context.Models.FindAsync(vehicleResource.ModelId);
+            // if (model == null)
+            // {
+            //     ModelState.AddModelError("ModelId", "Invalide modelId.");
+            //     return BadRequest(ModelState);
+            // }
+
             var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
-            return Ok(vehicle);
+            vehicle.LastUpdate = DateTime.Now;
+
+            context.Vehicles.Add(vehicle);
+            await context.SaveChangesAsync();
+
+            var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(result);
         }
     }
 }
