7bef1cddf7fc83243353dfccbd5ecd080d4ce35f
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-05-30 09:59:09 -0300

    CRUD API

---
+++ vega/Migrations/20180530122009_VehiclesNameTable.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace vega.Migrations
+{
+    public partial class VehiclesNameTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vahicles_Models_ModelId",
+                table: "Vahicles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleFeatures_Vahicles_VehicleId",
+                table: "VehicleFeatures");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vahicles",
+                table: "Vahicles");
+
+            migrationBuilder.RenameTable(
+                name: "Vahicles",
+                newName: "Vehicles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vahicles_ModelId",
+                table: "Vehicles",
+                newName: "IX_Vehicles_ModelId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleFeatures_Vehicles_VehicleId",
+                table: "VehicleFeatures",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vehicles_Models_ModelId",
+                table: "Vehicles",
+                column: "ModelId",
+                principalTable: "Models",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VehicleFeatures_Vehicles_VehicleId",
+                table: "VehicleFeatures");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vehicles_Models_ModelId",
+                table: "Vehicles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Vehicles",
+                table: "Vehicles");
+
+            migrationBuilder.RenameTable(
+                name: "Vehicles",
+                newName: "Vahicles");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Vehicles_ModelId",
+                table: "Vahicles",
+                newName: "IX_Vahicles_ModelId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Vahicles",
+                table: "Vahicles",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vahicles_Models_ModelId",
+                table: "Vahicles",
+                column: "ModelId",
+                principalTable: "Models",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VehicleFeatures_Vahicles_VehicleId",
+                table: "VehicleFeatures",
+                column: "VehicleId",
+                principalTable: "Vahicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- vega/Models/Vehicle.cs
+++ vega/Models/Vehicle.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace vega.Models
 {
-    [Table("Vahicles")]
+    [Table("Vehicles")]
     public class Vehicle
     {
         public int Id { get; set; }
