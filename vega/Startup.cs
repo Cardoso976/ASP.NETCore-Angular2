73c5885bc9ea127a494de0141fc5ed0fdf6f3ccd
Rodrigo Cardoso <rodrigo.ti@mfrural.com.br>
2018-06-06 08:30:01 -0300

    Photos serverside

--- vega/Startup.cs
+++ vega/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using vega.Core;
+using vega.Core.Models;
 using vega.Persistence;
 
 
@@ -27,6 +28,7 @@ namespace vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<PhotoSettings>(Configuration.GetSection("PhotoSettings"));
             services.AddScoped<IVehicleRepository, VehicleRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
